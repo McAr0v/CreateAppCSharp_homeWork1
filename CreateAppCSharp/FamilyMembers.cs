2e5e7762432d6fb66e6388b5051d3017d382546d
McAr0v <dvijkz@gmail.com>
2023-11-29 23:44:23 +0600

    Добавьте файлы проекта.

---
+++ CreateAppCSharp/FamilyMembers.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAppCSharp
+{
+    public class FamilyMembers
+    {
+        public int Age { get; set; }
+        public string FullName { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public FamilyMembers Mother { get; set; }
+        public FamilyMembers Father { get; set; }
+        public FamilyMembers Husband { get; set; }
+        public FamilyMembers Wife { get; set; }
+
+        // Метод создания члена семьи
+        public static FamilyMembers CreateMember(
+            string name,
+            Gender gender,
+            int age,
+            FamilyMembers? mother = null,
+            FamilyMembers? father = null,
+            FamilyMembers? husband = null,
+            FamilyMembers? wife = null
+
+            )
+        {
+
+            return new FamilyMembers { FullName = name, Gender = gender, Age = age, Mother = mother, Father = father, Husband = husband, Wife = wife };
+
+        }
+
+        // Метод получения данных о бабушках
+        public FamilyMembers[] GetGrandMother()
+        {
+            return new FamilyMembers[] { Mother?.Mother, Father?.Mother };
+
+        }
+
+        // Метод получения данных о дедушках
+        public FamilyMembers[] GetGrandFather()
+        {
+            return new FamilyMembers[] { Father?.Father, Mother?.Father };
+
+        }
+
+        // Метод получения данных о родителях
+        public FamilyMembers[] GetParents()
+        {
+            return new FamilyMembers[] { Father, Mother };
+
+        }
+
+        // Метод получения мужа/жены
+        public FamilyMembers[] Merried()
+        {
+            return new FamilyMembers[] { Husband, Wife };
+
+        }
+
+        // Метод отображения мужа/жены в консоль
+        public static void ShowMerried(FamilyMembers member)
+        {
+            // Получаем мужа/жену
+            var merried = member.Merried();
+
+            // Показываем в консоли
+
+            // В заголовке - member.FullName - кто женат, GetGenderMerriedWord(member.Gender) - метод получения
+            // фразы - женат на, замужем за - в зависимости от гендера
+
+            ShowInConsoleArray(
+                merried,
+                // Формируем заголовок для красоты
+                $"{member.FullName} {GetGenderMerriedWord(member.Gender)}");
+
+        }
+
+        // Метод перевода Man и Woman на русский
+        public static string GetGenderName(Gender gender) {
+
+            if (gender == Gender.man) return "Мужской";
+            else return "Женский";
+
+        }
+
+        // Метод для заголовка. Возвращает фразу в зависимости от гендера
+        public static string GetGenderMerriedWord(Gender gender)
+        {
+
+            if (gender == Gender.man) return "женат на";
+            else return "замужем за";
+
+        }
+
+
+        // Метод показа одного члена семьи в консоль
+        public static void ShowInConsole (FamilyMembers member, string headline = "")
+        {
+            if (member != null)
+            {
+                if (headline != "") Console.WriteLine($"{headline} -------------");
+                Console.WriteLine($"Имя - {member.FullName}, Возраст - {member.Age}, Пол - {GetGenderName(member.Gender)}");
+            }
+            
+        }
+
+        // Метод показа в консоль массива с членами семьи
+        public static void ShowInConsoleArray(FamilyMembers[] array, string headline = "")
+        {
+            Console.WriteLine($"{headline} -------------");
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                ShowInConsole (array[i]);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ CreateAppCSharp/Program.cs
@@ -0,0 +1,110 @@
+﻿/* 
+
+ * Доработайте приложение генеалогического дерева таким 
+ * образом чтобы программа выводила на экран близких родственников (жену/мужа). 
+ * Продумайте способ более красивого вывода с использованием горизонтальных и вертикальных черточек.
+ 
+ */
+using System.IO.MemoryMappedFiles;
+
+namespace CreateAppCSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Я создал метод, который создает нового члена семьи
+        // Таким методом я создал всех основных членов - 2 бабушки, 2 дедушки, отец, мать, дочь
+
+        FamilyMembers grandFatherOne = FamilyMembers.CreateMember(
+            name: "Макаров Петр",
+            gender: Gender.man,
+            age: 61
+            );
+
+        FamilyMembers grandFatherTwo = FamilyMembers.CreateMember(
+            name: "Беисов Нурлан",
+            gender: Gender.man,
+            age: 61
+            );
+
+        FamilyMembers grandMotherOne = FamilyMembers.CreateMember(
+            name: "Макарова Ольга",
+            gender: Gender.woman,
+            age: 60
+            );
+
+        FamilyMembers grandMotherTwo = FamilyMembers.CreateMember(
+            name: "Беисова Любовь",
+            gender: Gender.woman,
+            age: 55
+            );
+
+        FamilyMembers father = FamilyMembers.CreateMember(
+            name: "Макаров Егор",
+            gender: Gender.man,
+            age: 32,
+            mother: grandMotherOne,
+            father: grandFatherOne
+            );
+
+        FamilyMembers mother = FamilyMembers.CreateMember(
+            name: "Макарова Жанна",
+            gender: Gender.woman,
+            age: 33,
+            mother: grandMotherTwo,
+            father: grandFatherTwo
+            );
+
+
+        FamilyMembers daughter = FamilyMembers.CreateMember(
+            name: "Макарова Майя",
+            gender: Gender.woman,
+            age: 7,
+            mother: mother,
+            father: father
+            );
+
+        // Присваиваю пользователям мужей/жен
+
+        mother.Husband = father;
+        father.Wife = mother;
+        grandMotherOne.Husband = grandFatherOne;
+        grandFatherOne.Wife = grandMotherOne;
+        grandMotherTwo.Husband= grandFatherTwo;
+        grandFatherTwo.Wife = grandMotherTwo;
+
+
+        // Создаю массив со всеми членами семьи
+
+        FamilyMembers[] membersArray = new FamilyMembers[] { grandFatherOne, grandFatherTwo,  grandMotherOne, grandMotherTwo, father, mother, daughter};
+
+        // Я сделал метод для вывода в консоль данных из массива
+        
+        FamilyMembers.ShowInConsoleArray( membersArray, headline: "Все члены семьи" );
+
+        // Получаем бабушек, дедушек и родителей
+
+        var grandMothers = daughter.GetGrandMother();
+        var grandFathers = daughter.GetGrandFather();
+        var parents = daughter.GetParents();
+
+        // Выводим в консоль 
+
+        FamilyMembers.ShowInConsoleArray(grandMothers, headline: "БАБУЛИ");
+        FamilyMembers.ShowInConsoleArray(grandFathers, headline: "ДЕДУЛИ");
+        FamilyMembers.ShowInConsoleArray(parents, headline: "РОДИТЕЛИ");
+
+        // Выводим в консоль всех, кто на ком женат
+
+        for (int i = 0; i < membersArray.Length; i++)
+        {
+            FamilyMembers.ShowMerried(membersArray[i] );
+        }
+    }
+}
+
+
+
+public enum Gender { man, woman }
+
